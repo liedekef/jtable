22f916e3e915c7376572425b862c2583170b11c3
Franky Van Liedekerke <franky@e-dynamics.be>
2024-12-19 12:05:23 +0100

    theme updates

--- themes/update_css.sh
+++ themes/update_css.sh
@@ -3,6 +3,6 @@
 for i in `find . -name '*.less'|grep -v base.less`; do
     basedir=`dirname $i`
     basefile=`basename $i .less`
-    lessc $i > $basedir/$basefile.css
-    lessc -x $i > $basedir/$basefile.min.css
+    lessc -ru=all $i > $basedir/$basefile.css
+    lessc -ru=all -x $i > $basedir/$basefile.min.css
 done
