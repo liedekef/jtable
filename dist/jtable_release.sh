8b3cb0172864b96384830ee9f99be36b693c6766
Franky Van Liedekerke <franky@e-dynamics.be>
2024-12-20 16:50:43 +0100

    release script update

---
+++ dist/jtable_release.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+
+old_release=$1
+release=$2
+if [ -z "$release" ]; then
+       echo "Usage: $0 <old version number> <new version number>"
+       exit
+fi       
+
+# now update the release version
+scriptpath=$(realpath "$0")
+scriptdir=$(dirname $scriptpath)
+basedir=$(dirname $scriptdir)
+cd $basedir
+sed -i "s/$old_release/$release/" jquery.jtable.js
+sed -i "s/$old_release/$release/" CHANGES.md
+
+# now create a zip of the new release
+cd $basedir/..
+pwd
+zip -r jtable.zip jtable -x '*.git*' '*.less' -x 'jtable/dist*' -x 'jtable/*json' -x 'jtable/.npmignore' -x 'jtable/themes/update_css.sh'
+mv jtable.zip $basedir/dist/
+
+git commit -m "release $release" -a
+git push
+gh release create "v${release}" --generate-notes ./dist/*.zip
