3a83265932f19b1047ed4201405118a7a986a8d9
Franky Van Liedekerke <franky@e-dynamics.be>
2024-12-28 13:16:53 +0100

    release script update

--- script/jtable_release.sh
+++ script/jtable_release.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 
+old_release=$1
 release=$1
 if [ -z "$release" ]; then
        echo "Usage: $0 <old version number> <new version number>"
@@ -14,6 +15,7 @@ basedir=$(dirname $scriptdir)
 $scriptdir/update_css.sh
 $scriptdir/jtable_minify.sh
 echo $release >$basedir/VERSION
+sed -i "s/jTable $old_release/jTable $release/" jquery.jtable.js
 
 # now create a zip of the new release
 cd $basedir/..
