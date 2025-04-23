781d7920d438c9c26b8e0e5416e0f0cf3bc3c716
Franky Van Liedekerke <franky@e-dynamics.be>
2025-04-23 23:12:35 +0200

    release script update

--- script/jtable_minify.sh
+++ script/jtable_minify.sh
@@ -8,7 +8,5 @@ cd $basedir
 
 uglifyjs -o jquery.jtable.min.js jquery.jtable.js
 cd extensions
-uglifyjs -o jquery.jtable.aspnetpagemethods.min.js jquery.jtable.aspnetpagemethods.js
-uglifyjs -o jquery.jtable.localstorage.min.js jquery.jtable.localstorage.js
 uglifyjs -o jquery.jtable.record-actions.min.js jquery.jtable.record-actions.js
 uglifyjs -o jquery.jtable.toolbarsearch.min.js jquery.jtable.toolbarsearch.js
