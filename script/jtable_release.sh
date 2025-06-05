71ff1e454398c1a0979df06a50df0204d53e562d
Franky Van Liedekerke <franky@e-dynamics.be>
2025-06-05 23:30:39 +0200

    release 3.0.0

--- script/jtable_release.sh
+++ script/jtable_release.sh
@@ -7,24 +7,37 @@ if [ -z "$release" ]; then
        exit
 fi       
 
-# now update the release version
+# Set up paths
 scriptpath=$(realpath "$0")
 scriptdir=$(dirname $scriptpath)
 basedir=$(dirname $scriptdir)
 
+# Update/minify CSS and JS
 $scriptdir/update_css.sh
 $scriptdir/jtable_minify.sh
+
+# Update version file, JS header, and package.json
 echo $release >$basedir/VERSION
 sed -i "s/jTable $old_release/jTable $release/" jquery.jtable.js
 
-# now create a zip of the new release
+# --- NPM: update version and publish ---
+if [ -f "$basedir/package.json" ]; then
+    # Update package.json version (no git tag, since we'll handle it manually)
+    npm version $release --no-git-tag-version
+
+    # Publish to npm (scoped package, so --access public)
+    npm publish --access public
+fi
+
+# --- GitHub release steps as before ---
+# Create a zip of the new release for GitHub (but not for npm)
 cd $basedir/..
 pwd
 zip -r jtable.zip jtable -x '*.git*' '*.less' -x 'jtable/dist*' -x 'jtable/script*' -x 'jtable/*json' -x 'jtable/.npmignore' -x 'jtable/themes/update_css.sh'
 mv jtable.zip $basedir/dist/
 
 cd $basedir
-git add VERSION jquery.jtable.min.js
+git add VERSION jquery.jtable.min.js package.json
 git commit -m "release $release" -a
 git push
 gh release create "v${release}" --generate-notes ./dist/*.zip
