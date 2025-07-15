47b6a5430ae1dd76b05976ba2f9568f27b388764
Franky Van Liedekerke <franky@e-dynamics.be>
2025-07-15 12:35:36 +0200

    toolbar extension is now in base code

--- script/jtable_minify.sh
+++ script/jtable_minify.sh
@@ -7,6 +7,6 @@ basedir=$(dirname $scriptdir)
 cd $basedir
 
 uglifyjs -o jquery.jtable.min.js jquery.jtable.js
-cd extensions
-uglifyjs -o jquery.jtable.record-actions.min.js jquery.jtable.record-actions.js
-uglifyjs -o jquery.jtable.toolbarsearch.min.js jquery.jtable.toolbarsearch.js
+#cd extensions
+#uglifyjs -o jquery.jtable.record-actions.min.js jquery.jtable.record-actions.js
+#uglifyjs -o jquery.jtable.toolbarsearch.min.js jquery.jtable.toolbarsearch.js
